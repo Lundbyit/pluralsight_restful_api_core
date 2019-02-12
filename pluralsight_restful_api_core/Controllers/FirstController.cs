8ee38dac0f3140dfdb6a89f599360fe75694f7f8
LEE Toni <toni.lee@soprasteria.com>
2019-02-12 15:48:06 +0100

    setup starter project

---
+++ pluralsight_restful_api_core/Controllers/FirstController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace pluralsight_restful_api_core.Controllers
+{
+    [Route("api/test")]
+    public class FirstController : Controller
+    {
+
+        public IActionResult Index()
+        {
+            return Ok("funkar");
+        }
+    }
+}
